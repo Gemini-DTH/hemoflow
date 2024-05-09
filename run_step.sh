6bb5f9529f907f231724a74a3c1da754d6982d87
Adam Nowak <74208510+youngdashu@users.noreply.github.com>
2024-05-09 14:06:55 +0200

    Update run_step.sh

--- run_step.sh
+++ run_step.sh
@@ -6,15 +6,11 @@
 #SBATCH -A {{ grant_id }}
 
 module load hdf5
-module load cmake
 
 {% stage_in config %}
 {% stage_in flowrate %}
 {% stage_in vox %}
 
-mkdir build
 cd build
-cmake
-make -j 10
 
 mpirun -n 30 ./hemoFlow ../NAP_122_L01_F03_PA64_5_CFD.xml
