63aa3da0cd5d9673b74b91c9ef0d81cc1f3bdbe4
Adam Nowak <74208510+youngdashu@users.noreply.github.com>
2024-05-20 17:34:18 +0200

    Update run_step.sh

--- run_step.sh
+++ run_step.sh
@@ -14,9 +14,9 @@ module load hdf5
 
 cd $SCRATCH/hemoflow
 
-{% stage_in NAP_122_L01_F03_PA64_5_CFD.xml %} # config file
-{% stage_in cerebral_flowrate.txt %}
-{% stage_in vox_NAP_122_L01_F03_PA64_5c.npz %}
+{% stage_in_artifact NAP_122_L01_F03_PA64_5_CFD.xml %} # config file
+{% stage_in_artifact cerebral_flowrate.txt %}
+{% stage_in_artifact vox_NAP_122_L01_F03_PA64_5c.npz %}
 
 cd build
 
