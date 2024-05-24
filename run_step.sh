1b916e91f5e7009fb2a6b520f07d2852a803fc5c
Adam Nowak <74208510+youngdashu@users.noreply.github.com>
2024-05-24 19:53:05 +0200

    Update run_step.sh

--- run_step.sh
+++ run_step.sh
@@ -12,10 +12,14 @@ set -e
 module load hdf5
 module load cmake
 
-cd $SCRATCH/hemoflow/test_cases/run
+cd $SCRATCHDIR
+
+cp $SCRATCH/hemoflow/test_cases/run/. .
 
 mpirun -n 30 $SCRATCH/hemoflow/build/hemoFlow ./NAP_122_L01_F03_PA64_5_CFD.xml
 
 cd output_*
+
+ls -la
 
 {% stage_out hemoflow_output %}
