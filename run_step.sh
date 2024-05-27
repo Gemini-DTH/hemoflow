aab8f8c44a4d9da0c551d3b1e6e1ae98e2c13485
Adam Nowak <74208510+youngdashu@users.noreply.github.com>
2024-05-27 16:05:13 +0100

    Update run_step.sh

--- run_step.sh
+++ run_step.sh
@@ -12,6 +12,32 @@ set -e
 module load hdf5
 module load cmake
 
+cd $SCRATCH
+pwd
+
+if [[ -d "hemoflow" ]]
+then
+    echo "hemoflow directory exists on your scratch."
+    cd hemoflow
+    git pull
+else
+    ## Clone repository and switch into selected revision
+    echo Preparing computation source code
+    {% clone_repo %}
+    cd hemoflow
+fi
+
+if [[ -d "build" ]]
+then
+    echo "Build dir already exists."
+else
+    mkdir build
+fi
+cd ./build
+
+cmake ..
+make -j 10
+
 cd $SCRATCHDIR
 
 cp -r $SCRATCH/hemoflow/test_cases/run/. .
