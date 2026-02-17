e2f9bafdc164b3aa11f1c372b8b2f2b0b0bb859b
Karol Zając <karol.zajac30@gmail.com>
2026-02-17 11:15:15 +0100

    Adapt EasyVVUQ experiment setup to Ares cluster

--- tests/uncertainty/1-uncertainty_ares_cluster_testing/uncertainty_quantification_test.py
+++ tests/uncertainty/1-uncertainty_ares_cluster_testing/uncertainty_quantification_test.py
@@ -5,16 +5,13 @@ import os
 import time
 import numpy as np
 import glob
+
 # For hemoflow use the feature-aneurysm branch
-SOFTWARE_PATH = "/home/nan/Documents/github/hemoflow/"
-SOFTWARE_PATH = os.path.abspath(SOFTWARE_PATH)
-# HEMOFLOW_PATH='/mnt/d/1_Github/VascuTreatCFD/hemoflowcfd/build/hemoflow'
-HEMOFLOW_PATH = os.path.join(SOFTWARE_PATH, "build", "hemoFlow")
-HEMOFLOW_PATH = os.path.abspath(HEMOFLOW_PATH)
+TMP_DIR = os.environ.get("TMPDIR")
+HEMOFLOW_PATH = os.environ.get("HEMOFLOW_PATH")
 
 # For LBMpost use the dev branch
-# LBMPOST_PATH = "/home/lsandor/00_software/LBMpost/main.py"
-LBMPOST_PATH = "/home/nan/Documents/github/LBMpost/main.py"
+LBMPOST_PATH = os.environ.get("LBMPOST_PATH")
 TEMPLATE_DIR_PATH = "campaign_dir"
 TEMPLATE_DIR_PATH = os.path.abspath(TEMPLATE_DIR_PATH)
 
@@ -54,9 +51,7 @@ def run_uncertainty_quantification(client_param):
         #!RUNNING ON 6 CORES
         uq.actions.ExecuteLocal("mpirun -n 6 " + HEMOFLOW_PATH + " input.xml"),
         # conda env for running LBMpost, livestream for stdio
-        uq.actions.ExecuteLocal(
-            "conda run --live-stream -n lbmpost python " + LBMPOST_PATH + " ./ full --purge"
-        ),
+        uq.actions.ExecuteLocal("python3 -u {} ./ full".format(LBMPOST_PATH)),
         uq.actions.Decode(decoder),
     )
 
@@ -71,7 +66,6 @@ def run_uncertainty_quantification(client_param):
     print(data_frame)
     data_frame.to_csv("uncertainty_output.csv")
 
-    time.sleep(5)  # why 5 sec?
 
 
 if __name__ == "__main__":
@@ -80,8 +74,7 @@ if __name__ == "__main__":
     Parsing arguments to specify type of run. Possible options:
     --local - running locally.
     --slurm - running using SLURM with default option dask-jobqueue - deploy Dask
-              on common job queuing systems on HPC. Dask-mpi option also available
-              for deploying Dask from within an existing MPI environment.
+              on common job queuing systems on HPC.
     """
     parser = argparse.ArgumentParser(
         description="EasyVVUQ applied to a vertical tube deflection (using DASK).",
@@ -95,7 +88,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--slurm",
         "-s",
-        help="Run using SLURM. Possible options: dask-jobqueue, dask-mpi.",
+        help="Run using SLURM. Possible options: dask-jobqueue",
         default="dask-jobqueue",
     )
 
@@ -112,49 +105,45 @@ if __name__ == "__main__":
         print("Running with SLURM using dask-jobqueue.")
         from dask_jobqueue import SLURMCluster
 
+        job_script_prologue = [
+            "set -e",
+            "module load hdf5",
+            "module load cmake",
+            "cd $TMPDIR",
+            "mkdir env_mountpoint",
+            "squashfuse $SCRATCH/lbmpost.squashfs env_mountpoint",
+            "conda activate $TMPDIR/env_mountpoint",
+        ]
+
         cluster = SLURMCluster(
             shebang="#!/bin/bash -l",
-            job_extra=[
-                "--output /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.out",
-                "--error /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.err",
-            ],
-            queue="plgrid-short",
-            project="plgsano2",
-            cores=24,
-            processes=8,
-            memory="20GB",
-            walltime="00:10:00",
+            queue="plgrid",
+            account="plggemini2026-cpu",
+            cores=1,
+            processes=1,
+            memory="16GB",
+            walltime="04:00:00",
             interface="ib0",
-            scheduler_options={"interface": "eth55"},
+            scheduler_options={"interface": "ib0"},
+            python="python3",
+            n_workers=1,
+            job_cpu=6,
+            job_script_prologue=job_script_prologue,
+            worker_extra_args=["--memory-limit 1GiB"],
+            job_extra_directives=[
+                "--output ./slurm-%j.out",
+                "--error ./slurm-%j.err",
+            ],
         )
+
         cluster.job_cls.submit_command = "sbatch"
         cluster.submit_command = "sbatch"
-        cluster.scale(jobs=8)
+        cluster.scale(jobs=int(os.environ.get("JOBS_NUM")))
         client = Client(cluster)
         print(cluster)
         print(client)
+
         run_uncertainty_quantification(client)
-    elif args.slurm == "dask-mpi":
-        print("Running with SLURM using dask-mpi.")
-        from dask_mpi import initialize
-        from distributed.scheduler import logger
-        import socket
-
-        initialize()
-        client = Client()
-
-        host = client.run_on_scheduler(socket.gethostname)
-        port = client.scheduler_info()["services"]["dashboard"]
-        login_node_address = "login@pro.cyfronet.pl"  # Change this to the address/domain of your login node
-
-        logger.info(f"ssh -N -L {port}:{host}:{port} {login_node_address}")
-
-        from dask.distributed import performance_report
-
-        with performance_report(filename="dask-report.html"):
-            run_uncertainty_quantification(client)
-
-        client.profile(filename="dask-profile.html")
 
     else:
         print("Incorrect slurm option specified!")
